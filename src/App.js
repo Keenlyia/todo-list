90180bfa6d29b4c03ebf66e0751f3a009b2a2be6
Keenly228 <Umkan.2002@gmail.com>
2025-05-03 23:01:26 +0300

    commits

--- src/App.js
+++ src/App.js
@@ -1,86 +1,86 @@
 import './App.css';
-import AddTodo from './components/AddTodo';
+import AddTodo from './components/AddTodo'
 import Todo from "./components/Todo";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect  } from "react";
 
-// Базова URL-адреса API на Heroku
-const apiUrl = 'https://backend-todo-list-19d05a62b7ba.herokuapp.com/';
 
 function App() {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState([])
 
     const getTodos = async () => {
         try {
-            const response = await axios.get(`${apiUrl}api/v1/todo/`);  // використовуємо apiUrl
-            const { data } = response;
-            setTodos(data);
+            const respnose = await axios.get('/api/v1/todo/')
+            const { data } = respnose
+            setTodos(data)
         } catch (err) {
-            console.log(err);
+            console.log(err)
         }
-    };
+    }
 
     useEffect(() => {
-        getTodos();
+        getTodos()
     }, []);
 
     const addTodo = async newTodo => {
         try {
-            console.log(newTodo);
-            await axios.post(`${apiUrl}api/v1/todo/`, newTodo);  // використовуємо apiUrl
-            getTodos();
+            console.log(newTodo)
+            await axios.post('api/v1/todo/', newTodo)
+            getTodos()
         } catch (err) {
-            console.log(err);
+            console.log(err)
         }
-    };
+    }
 
     const completeTodo = async id => {
         try {
-            const todo = todos.filter(todo => todo.id === id)[0];
-            todo.completed = true;
-            await axios.put(`${apiUrl}api/v1/todo/${id}/`, todo);  // використовуємо apiUrl
-            getTodos();
+            const todo = todos.filter(todo => todo.id === id)[0]
+            todo.completed = true
+            await axios.put(`/api/v1/todo/${id}/`, todo)
+            getTodos()
+
         } catch (err) {
-            console.log(err);
+            console.log(err)
         }
-    };
+    }
 
     const editTodo = async todo => {
         try {
-            await axios.put(`${apiUrl}api/v1/todo/${todo.id}/`, todo);  // використовуємо apiUrl
-            getTodos();
-        } catch (err) {
-            console.log(err);
+            await axios.put(`/api/v1/todo/${todo.id}/`, todo)
+            getTodos()
+        }catch (err) {
+            console.log(err)
         }
-    };
+    }
 
     const deleteTodo = async id => {
         try {
-            await axios.delete(`${apiUrl}api/v1/todo/${id}/`);  // використовуємо apiUrl
-            getTodos();
+            await axios.delete(`/api/v1/todo/${id}/`)
+            getTodos()
         } catch (err) {
-            console.log(err);
-        }
-    };
 
-    return (
-        <div className='wrapper'>
-            <Container>
-                <Row className='justify-content-center pt-5'>
-                    <Col>
-                        <Card className='p-5'>
-                            <h3>My Todos</h3>
-                            <AddTodo addTodo={addTodo} />
-                            {todos.map((todo, index) => (
-                                !todo.completed && <Todo key={index} id={todo.id} title={todo.title} description={todo.description} completeTodo={completeTodo} editTodo={editTodo} deleteTodo={deleteTodo} />
-                            ))}
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+        }
+    }
+
+
+  return (
+    <div className='wrapper'>
+        <Container>
+            <Row className='justify-content-center pt-5'>
+                <Col>
+                    <Card className='p-5'>
+                        <h3>My Todos</h3>
+                        <AddTodo addTodo={addTodo}/>
+                        {todos.map((todo, index) => (
+                            !todo.completed && <Todo key={index} id={todo.id} title={todo.title} description={todo.description} completeTodo={completeTodo} editTodo={editTodo} deleteTodo={deleteTodo}/>
+                        ))}
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    </div>
+  );
 }
 
 export default App;
